151c4ac9f69f281970cacc387f13aef59ab5c9e5
ali akkuş <alivehicran@hotmail.com>
2022-04-25 02:09:52 +0300

    Rest Api's Node Js Express Js Or MongoDb

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-const cors = require("cors")
+const express = require("express");
+var bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
@@ -11,34 +11,55 @@ app.use(express.static("public"));
 
 app.use(cors());
 
-var writelist=["http://localhost,http://aliakkus.com"]
+var writelist = ["http://localhost,http://aliakkus.com"];
 
-var corsOptions={origin:function(origin,callback){
-    if(writelist.indexOf(origin)!== -1){
-        callback(null,true)
-    }else{
-        callback(null,false)
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (writelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
     }
-},
-Credential:true,
+  },
+  Credential: true,
 };
-app.use(cors(corsOptions))
-let products=[]
+app.use(cors(corsOptions));
+let products = [];
 
-app.get("/api/products",req,res=>{
-    res.json(products);
+app.get("/api/products", req, (res) => {
+  res.json(products);
+  let data = {};
+  req.json(data);
 });
 
-app.post("/api/products",req, res=>{
-    let product ={};
-    product = req.body;
-    product.push({...product});
+app.post("/api/products", req, (res) => {
+  let product = {};
+  product = req.body;
+  product.push({ ...product });
 
-    res.json(products);
-})
+  res.json(products);
+});
 
-app.get('/',(req, res)=>{
-    res.json("Hoşgeldiniz Express Js Ana Sayfasına")
-})
-app.listen(process.env.port ||5055)
-console.log(" webServer listening at "+(process.env.port || 5055))
+app.put("/api/products", req, (res) => {
+  let product = {};
+  product = req.body;
+  product.map((prod, index) => {
+    if (prod.id == product.id) {
+      products[index] = { ...product };
+    }
+  });
+});
+app.delete("/api/products", (req, res) => {
+  let ID = 0;
+
+  ID = req.params.id;
+  products = products.filter((prod) => prod.id.toString() !== ID.toString());
+
+  res.json(products);
+});
+
+app.get("/", (req, res) => {
+  res.json("Hoşgeldiniz Express Js Ana Sayfasına");
+});
+app.listen(process.env.port || 5055);
+console.log(" webServer listening at " + (process.env.port || 5055));
